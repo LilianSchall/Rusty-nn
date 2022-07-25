eedc96ea29382a0da7ee339a0c4c644cd0cde30a
Lilian Schall <lilian.schall@epita.fr>
2022-07-26 02:34:49 +0900

    Adding simple structure for shape description of a dense neural network

---
+++ src/Shapes/dense_shape.rs
@@ -0,0 +1,19 @@
+pub struct DenseShape {
+    pub x: u32,
+    pub y: u32,
+    pub z: u32,
+    pub range: u32,
+}
+
+impl DenseShape {
+    pub fn new(x: u32, y: u32, z: u32) -> DenseShape {
+
+        DenseShape {
+            x: x,
+            y: y,
+            z: z,
+            range: x*y*z
+        };
+    }
+    
+}
