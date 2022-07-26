41fa105ba6119176971ac82656ad3ac5320ac01a
Lilian Schall <lilian.schall@epita.fr>
2022-07-26 22:46:29 +0900

    Made the states of the matrix struct public, added dot function, add function and add_real method to the matrix struct

--- src/maths/matrices.rs
+++ src/maths/matrices.rs
@@ -1,7 +1,7 @@
 pub struct Matrix {
-    x_length: usize,
-    y_length: usize,
-    values: Vec<f64>
+    pub x_length: usize,
+    pub y_length: usize,
+    pub values: Vec<f64>
 }
 
 impl Matrix {
@@ -16,5 +16,80 @@ impl Matrix {
         }
     }
 
+    pub fn copy(&self) -> Matrix {
+        let mut copied: Matrix = Matrix::new(self.x_length, self.y_length);
+
+        for i in 0..self.values.len() {
+            copied.values[i] = self.values[i];
+        }
+
+        copied
+    }
+
+    pub fn get(&self, y: usize, x: usize) -> f64 {
+        self.values[y * self.x_length + x]
+    }
+
+    pub fn set(&mut self, y: usize, x: usize, value: f64) {
+        if y >= self.y_length || x >= self.x_length {
+            return;
+        }
+
+        self.values[y * self.x_length + x] = value;
+    }
+
+    pub fn dot(mat1: &Matrix, mat2: &Matrix) -> Option<Matrix> {
+        if mat1.x_length != mat2.y_length {
+            return None;
+        }
+        let mut Z = Matrix::new(mat2.x_length, mat1.y_length);
+        
+        for y in 0..Z.y_length {
+            for x in 0..Z.x_length {
+                let mut value: f64= 0.0;
+                for n in 0..mat1.x_length { // or mat2.y_length 
+                    
+                    value += mat1.get(y,n) * mat2.get(n,x);
+                }
+                Z.set(y, x, value);
+            }
+        }
+
+        Some(Z)
+
+    }
+
+    pub fn add(mat1: &Matrix, mat2: &Matrix) -> Option<Matrix> {
+        if mat1.x_length != mat2.x_length || mat1.y_length != mat2.y_length {
+            return None;
+        }
+
+        let mut Z = Matrix::new(mat1.x_length, mat1.y_length);
+
+        for y in 0..Z.y_length {
+            for x in 0..Z.x_length { 
+                
+                Z.set(y, x, mat1.get(y,x) + mat2.get(y, x));
+            }
+        }
+
+        Some(Z)
+    }
+
+    pub fn vec_to_col_mat(vec: Vec<f64>) -> Matrix{
+        let mut result = Matrix::new(1, vec.len());
+
+        for i in 0..vec.len() {
+            result.values[i] = vec[i];
+        }
+
+        result
+    }
+
+    pub fn add_real(&mut self, b: f64) {
+        for i in 0..self.values.len() {
+            self.values[i] += b;
+        }
+    }
     
 }
--- src/maths/mod.rs
+++ src/maths/mod.rs
@@ -1,1 +1,3 @@
 pub mod matrices;
+
+pub const INFINITY: f64 = 100000000.0;
