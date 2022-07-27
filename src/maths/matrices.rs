bd76f61400669edbc6c6340ce12ffc7e56530d5e
Lilian Schall <lilian.schall@epita.fr>
2022-07-28 02:17:38 +0900

    Modified the dot and add product exception handling, now the process is exited with a debug log about the different states of the matrices involved. Added also a copy method and a shuffle method which copies the content of a matrix or fill the matrix with random values

--- src/maths/matrices.rs
+++ src/maths/matrices.rs
@@ -1,3 +1,6 @@
+use rand::Rng;
+use std::process;
+
 pub struct Matrix {
     pub x_length: usize,
     pub y_length: usize,
@@ -7,13 +10,30 @@ pub struct Matrix {
 impl Matrix {
     pub fn new(x_length: usize, y_length: usize) -> Matrix {
 
-        let values = Vec::with_capacity(x_length * y_length);
+        let size: usize = x_length * y_length;
+
+        let mut values = Vec::with_capacity(size);
+
+        for _ in 0..size{
+            values.push(0.0);
+        } 
 
         Matrix {
             x_length: x_length,
             y_length: y_length,
             values: values
         }
+    }
+
+    pub fn shuffle(mut self) -> Matrix {
+        
+        let mut rng = rand::thread_rng();
+
+        for i in 0.. self.values.len() {
+            self.values[i] = rng.gen::<f64>();
+        }
+
+        self
     }
 
     pub fn copy(&self) -> Matrix {
@@ -27,20 +47,46 @@ impl Matrix {
     }
 
     pub fn get(&self, y: usize, x: usize) -> f64 {
+        if y >= self.y_length || x >= self.x_length {
+            let ymax = self.y_length;
+            let xmax = self.x_length;
+
+            println!("Error: GET method for matrix has encountered an exception");
+            println!("Expected to have a y or a x lower then y_length or x_length");
+            println!("--------DEBUG------------");
+            println!("y: {y}, x: {x}, |");
+            println!("y_length: {ymax}, x_length: {xmax}");
+            process::exit(1);
+        }
         self.values[y * self.x_length + x]
     }
 
     pub fn set(&mut self, y: usize, x: usize, value: f64) {
         if y >= self.y_length || x >= self.x_length {
-            return;
+            let ymax = self.y_length;
+            let xmax = self.x_length;
+
+            println!("Error: SET method for matrix has encountered an exception");
+            println!("Expected to have a y or a x lower then y_length or x_length");
+            println!("--------DEBUG------------");
+            println!("y: {y}, x: {x}");
+            println!("y_length: {ymax}, x_length: {xmax}");
+            process::exit(1);
         }
 
         self.values[y * self.x_length + x] = value;
     }
 
-    pub fn dot(mat1: &Matrix, mat2: &Matrix) -> Option<Matrix> {
+    pub fn dot(mat1: &Matrix, mat2: &Matrix) -> Matrix {
         if mat1.x_length != mat2.y_length {
-            return None;
+            let mat1x = mat1.x_length;
+            let mat2y = mat2.y_length;
+
+            println!("Error: DOT function for matrix has encountered an exception");
+            println!("Expected to have a two matrices of y size = x size");
+            println!("--------DEBUG------------");
+            println!("mat1x: {mat1x}, mat2y: {mat2y}");
+            process::exit(1);
         }
         let mut Z = Matrix::new(mat2.x_length, mat1.y_length);
         
@@ -55,25 +101,36 @@ impl Matrix {
             }
         }
 
-        Some(Z)
+        Z
 
     }
 
-    pub fn add(mat1: &Matrix, mat2: &Matrix) -> Option<Matrix> {
+    pub fn add(mat1: &Matrix, mat2: &Matrix) -> Matrix {
         if mat1.x_length != mat2.x_length || mat1.y_length != mat2.y_length {
-            return None;
+            let mat1x = mat1.x_length;
+            let mat1y = mat1.y_length;
+
+            let mat2x = mat2.x_length;
+            let mat2y = mat2.y_length;
+
+            println!("Error: ADD function for matrix has encountered an exception");
+            println!("Expected to have a two matrices of same size");
+            println!("--------DEBUG------------");
+            println!("mat1x: {mat1x}, mat1y: {mat1y}");
+            println!("mat2x: {mat2x}, mat2y: {mat2y}");
+
+            process::exit(1);
         }
 
         let mut Z = Matrix::new(mat1.x_length, mat1.y_length);
 
         for y in 0..Z.y_length {
             for x in 0..Z.x_length { 
-                
                 Z.set(y, x, mat1.get(y,x) + mat2.get(y, x));
             }
         }
 
-        Some(Z)
+        Z
     }
 
     pub fn vec_to_col_mat(vec: Vec<f64>) -> Matrix{
